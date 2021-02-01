7f9a39ffe0b6ff3c6595727f1eaa393b2ab71abe
Dehantor <dehantor@gmail.com>
2021-02-01 10:55:15 +0300

    Добавьте файлы проекта.

---
+++ CowCount/CowExcel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using OfficeOpenXml;
+using System.Diagnostics;
+
+namespace CowCount
+{
+    class CowExcel
+    {
+        /// <summary>
+        /// Агрегация данных с csv
+        /// </summary>
+        /// <param name="file"></param>
+        public void Start(FileInfo file)
+        {
+            try
+            {
+                //проблема с кодировкой
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                //открывем ексель куда будем сохранять все данные
+                using (var package = new ExcelPackage(file))
+                {
+                    ExcelWorksheet WS;
+                    //копируем данные с шаблона
+                    try
+                    {
+                        using (var temporary = new ExcelPackage(new FileInfo("Шаблон.xlsx")))
+                        {
+                            WS = package.Workbook.Worksheets.Add(DateTime.Now.ToString("dd.MM.yyyy"), temporary.Workbook.Worksheets["шаблон"]);
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Лист с таками именем уже существует");
+                        return;
+                    }
+                    //проходимся по всем файлам CSV и копируем данные из них
+                    string direct = Environment.CurrentDirectory;
+                    DirectoryInfo directoryInfo = new DirectoryInfo(direct);
+                    foreach (FileInfo item in directoryInfo.GetFiles())
+                    {
+                        if (item.Name.Contains(".CSV"))
+                        {
+                            int qual;
+                            var lines = File.ReadAllLines(item.Name, Encoding.GetEncoding("windows-1251"));
+                            if (lines[0].Split(";")[1] == "\"р  ДСП\"")
+                                qual = Convert.ToInt32(lines[1].Split(";")[1]);
+                            else
+                                qual = Convert.ToInt32(lines[1].Split(";")[0]);
+                            string cell = item.Name.Replace(".CSV", "");
+                            WS.Cells[cell].Value = qual;
+                        }
+                    }
+                    //сохраняем файл
+                    package.Save();
+                    //открываем ексель файл
+                    var proc = new Process();
+                    proc.StartInfo = new ProcessStartInfo(file.Name)
+                    {
+                        UseShellExecute = true
+                    };
+                    proc.Start();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ CowCount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CowCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fileName;
+            while (true)
+            {
+                Console.WriteLine("Введите название файлас расширением: ");
+                string file = Console.ReadLine();
+                fileName = new FileInfo(file);
+                if (fileName.Exists)
+                    break;
+                else
+                    Console.WriteLine("Файл не существеует! попробуйте еще раз.");
+            }
+            CowExcel cowExcel = new CowExcel();
+            cowExcel.Start(fileName);
+            Console.WriteLine("Готово");
+        }
+    }
+}
